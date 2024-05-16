62971452f380764883e50f17bc8adc98032be5ff
MDW-Guillaume <guillaume.couvidou@mozartsduweb.com>
2024-05-16 12:52:33 +0200

    Router installation + Header & Footer / Fonts & Images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Components/Partials/Header';
+import Footer from './Components/Partials/Footer';
+import Home from './Components/Home/Home';
+import './index.css';
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Components/Partials/Footer.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
+import logo from '../../Images/logo-white.svg';
+import dribbble from '../../Images/Icons/dribbble.svg';
+import facebook from '../../Images/Icons/facebook.svg';
+import instagram from '../../Images/Icons/instagram.svg';
+import linkedin from '../../Images/Icons/linkedin.svg';
+import twitter from '../../Images/Icons/twitter.svg';
+
+export default function Footer() {
+    return (
+        <footer className="bg-blue-night pt-16 text-white">
+            <div className="container">
+                <div className="flex justify-between mb-28">
+                    <div className="flex flex-col w-5/12">
+                        <img src={logo} alt="Bourse aux Stages" className="logo w-3/4 mb-3"></img>
+                        <p className="text-white-light">Première plateforme dédiée à la recherche de <br/>stages et d’alternance qui relie automatiquement <br/>les étudiants et les entreprises.</p>
+                    </div>
+                    <div className="flex w-4/12 gap-12 text-white-light">
+                        <ul className="flex flex-col gap-4">
+                            <li><Link to="#">Accueil</Link></li>
+                            <li><Link to="#">Offres</Link></li>
+                            <li><Link to="#">Demandes</Link></li>
+                            <li><Link to="#">Entreprises</Link></li>
+                            <li><Link to="#">Etudiants</Link></li>
+                        </ul>
+                        <ul className="flex flex-col gap-4">
+                            <li><Link to="#">Blog</Link></li>
+                            <li><Link to="#">Sponsors</Link></li>
+                            <li><Link to="#">Mentions légales</Link></li>
+                            <li><Link to="#">Données personnelles</Link></li>
+                            <li><Link to="#">Nous contacter</Link></li>
+                        </ul>
+                    </div>
+                    <div className="flex flex-col w-4/12 ">
+                        <p className="mb-4">Étudiants, créez votre compte</p>
+                        <p className="text-white-light mb-10">Recevez automatiquement par email <br/>les offres qui vous intéressent !</p>
+                        <div className="flex gap-2">
+                            <input
+                                type="email"
+                                name="email"
+                                className="px-4 py-3"
+                                placeholder="Votre email"
+                            />
+                            <button type="button" className="btn-blue-dark">C'est parti</button>
+                        </div>
+                    </div>
+                </div>
+                <hr className="opacity-50 h-0.5"></hr>
+                <div className="flex justify-between py-10">
+                    <span className="opacity-50	">2024 @  Bourse aux Stages • Tous droits réservés</span>
+                    <div className="flex justify-between w-3/12">
+                        <Link to="#" className="bubble-dark flex justify-center rounded-full p-2.5 w-8"><img src={dribbble}/></Link>
+                        <Link to="#" className="bubble-dark flex justify-center rounded-full p-2.5 w-8"><img src={facebook}/></Link>
+                        <Link to="#" className="bubble-dark flex justify-center rounded-full p-2.5 w-8"><img src={instagram}/></Link>
+                        <Link to="#" className="bubble-dark flex justify-center rounded-full p-2.5 w-8"><img src={linkedin}/></Link>
+                        <Link to="#" className="bubble-dark flex justify-center rounded-full p-2.5 w-8"><img src={twitter}/></Link>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
