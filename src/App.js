b184c6ad1414add2e922e20e81da19160f301ffc
MDW-Guillaume <guillaume.couvidou@mozartsduweb.com>
2024-05-27 01:28:58 +0200

    ADD Companies front page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './Components/Partials/Footer';
 import Home from './Components/Home/Home';
 import StageOffers from './Components/Stage/Offers';
 import ApprenticeOffers from './Components/Apprentice/Offers';
+import CompanyList from './Components/Company/List';
 import './index.css';
 import {Routes, Route} from "react-router-dom";
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/stage/offres" element={<StageOffers />} />
           <Route path="/alternance/offres" element={<ApprenticeOffers />} />
+          <Route path="/entreprises" element={<CompanyList />} />
         </Routes>
 
       <Footer />
