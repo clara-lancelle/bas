07751ef2c13106604aa80e45eaae3caa4cb3ea1b
MDW-Guillaume <guillaume.couvidou@mozartsduweb.com>
2024-06-29 18:57:48 +0200

    ADD CompanyUserAccount + forms

--- src/Routes/Restricted.js
+++ src/Routes/Restricted.js
@@ -1,5 +1,6 @@
 import BackofficeCompanyOffers from '../Components/Backoffice/CompanyOffers';
 import BackofficeUserAccount from '../Components/Backoffice/UserAccount';
+import BackofficeCompanyUserAccount from '../Components/Backoffice/Company/CompanyUserAccount';
 import BackofficeCompanyAccount from '../Components/Backoffice/CompanyAccount';
 
 export const restrictedRoutes = [
@@ -14,7 +15,12 @@ export const restrictedRoutes = [
         restricted: true,
         props: { userInfo: {}, notify: () => {} },
     },
-
+    {
+        path: '/backoffice/mon-compte',
+        element: <BackofficeUserAccount />,
+        restricted: true,
+        props: { userInfo: {}, notify: () => {} },
+    },
     {
         path: '/backoffice/mon-organisation',
         element: <BackofficeCompanyAccount />,
